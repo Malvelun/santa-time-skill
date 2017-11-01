95eec07cf8bd8402cf17e2929a3430e08c22a27a
Matthias <m.blohm@etermio.com>
2017-11-01 21:42:34 +0100

    Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // ALWAYS setup the alexa app and attach it to express before anything else.
-var alexaApp = new alexa.app("test");
+var alexaApp = new alexa.app("santa-time");
 
 alexaApp.express({
   expressApp: app,
@@ -14,17 +14,24 @@ alexaApp.express({
   // verifies requests come from amazon alexa. Must be enabled for production.
   // You can disable this if you're running a dev environment and want to POST
   // things to test behavior. enabled by default.
-  checkCert: false,
+  checkCert: true,
 
   // sets up a GET route when set to true. This is handy for testing in
   // development, but not recommended for production. disabled by default
-  debug: true
+  debug: false
 });
 
 // now POST calls to /test in express will be handled by the app.request() function
 
 // from here on you can setup any other express routes or middlewares as normal
 app.set("view engine", "ejs");
+
+app.pre = function(request, response, type) {
+  if (request.applicationId != "amzn1.ask.skill.0471c64a-5dd7-4470-ad36-03134c6c68bc") {
+    // fail ungracefully
+    return response.fail("Invalid Application ID");
+  }
+};
 
 alexaApp.launch(function(request, response) {
   response.say("Welcome to Santa Time!");
